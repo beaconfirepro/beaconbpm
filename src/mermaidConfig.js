3756c3b1555c29dca35d9214ac9c39e86e79029c
beaconfirepro <ddale@beaconfire.co>
2025-07-19 14:25:34 +0000

    Set Mermaid diagrams to use bold font and white text explicitly

--- src/mermaidConfig.js
+++ src/mermaidConfig.js
@@ -1,19 +1,19 @@
-// src/mermaidConfig.js
 const mermaidConfig = {
   startOnLoad: true,
   theme: 'base',
   themeVariables: {
     fontFamily: "'Evolve Sans', sans-serif",
+    fontWeight: "bold",
     fontSize: '16px',
     primaryColor: '#114B5F',
     secondaryColor: '#028090',
     tertiaryColor: '#E06F03',
-    textColor: '#1b204a',
+    textColor: '#ffffff', // explicitly white text
     lineColor: '#6B8F9C',
     nodeBorder: '#114B5F',
     clusterBkg: '#F1EEEC',
     edgeLabelBackground: '#ffffff',
-    nodeTextColor: '#191D4D',
+    nodeTextColor: '#ffffff', // white text in nodes explicitly
     nodeBorderRadius: '8px',
   },
   flowchart: {
