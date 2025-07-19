743e4a7a7f07bdec469a3c47f24188bc9cbdbfc8
beaconfirepro <ddale@beaconfire.co>
2025-07-19 14:46:40 +0000

    Add Mermaid diagram PNG download functionality

--- src/MermaidEditor.js
+++ src/MermaidEditor.js
@@ -5,6 +5,7 @@ import mermaidConfig from './mermaidConfig';
 const MermaidEditor = () => {
   const [code, setCode] = useState('');
   const diagramRef = useRef(null);
+  const [svgCode, setSvgCode] = useState('');
 
   useEffect(() => {
     mermaid.initialize(mermaidConfig);
@@ -13,13 +14,39 @@ const MermaidEditor = () => {
   const handleRender = () => {
     if (diagramRef.current) {
       mermaid.render('mermaidDiagram', code)
-        .then(({svg}) => {
+        .then(({ svg }) => {
           diagramRef.current.innerHTML = svg;
+          setSvgCode(svg);
         })
         .catch((error) => {
           diagramRef.current.innerHTML = `<pre style="color:red;">${error}</pre>`;
         });
     }
+  };
+
+  const downloadImage = () => {
+    const svgBlob = new Blob([svgCode], { type: 'image/svg+xml;charset=utf-8' });
+    const svgUrl = URL.createObjectURL(svgBlob);
+    const img = new Image();
+
+    img.onload = () => {
+      const canvas = document.createElement('canvas');
+      canvas.width = img.width;
+      canvas.height = img.height;
+
+      const context = canvas.getContext('2d');
+      context.drawImage(img, 0, 0);
+
+      URL.revokeObjectURL(svgUrl);
+      canvas.toBlob((blob) => {
+        const a = document.createElement('a');
+        a.href = URL.createObjectURL(blob);
+        a.download = 'mermaid-diagram.png';
+        a.click();
+      });
+    };
+
+    img.src = svgUrl;
   };
 
   return (
@@ -33,6 +60,9 @@ const MermaidEditor = () => {
       />
       <button onClick={handleRender} style={{ marginTop: '10px', padding: '8px 15px' }}>
         Render Diagram
+      </button>
+      <button onClick={downloadImage} style={{ marginTop: '10px', marginLeft: '10px', padding: '8px 15px' }}>
+        Download as Image
       </button>
       <div ref={diagramRef} style={{ marginTop: '20px' }} />
     </div>
